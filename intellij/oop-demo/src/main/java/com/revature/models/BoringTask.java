a8b38c26d4129ca8c387d5deecbf707a9977a390
Albert Lee <albert.lee@alumni.stevens.edu>
2021-10-20 16:10:30 -0400

    procrastinate method working

---
+++ intellij/oop-demo/src/main/java/com/revature/models/BoringTask.java
@@ -0,0 +1,13 @@
+package com.revature.models;
+
+import java.time.LocalDate;
+
+public class BoringTask extends Task{
+    public BoringTask(String name, LocalDate dueDate){
+        super(name, dueDate);
+    }
+
+    public void procrastinate() {
+        this.setDueDate(this.getDueDate().plusDays(1));
+    }
+}
