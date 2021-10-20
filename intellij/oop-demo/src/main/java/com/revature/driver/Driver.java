f5d2193bcd0b83ab231deae51f5aaaad3f2090b5
Albert Lee <albert.lee@alumni.stevens.edu>
2021-10-20 16:15:01 -0400

    fix intellij setup

--- intellij/oop-demo/src/main/java/com/revature/driver/Driver.java
+++ intellij/oop-demo/src/main/java/com/revature/driver/Driver.java
@@ -4,11 +4,9 @@ import com.revature.services.OopService;
 
 public class Driver {
 
-	private static scanner = new Scanner();
-
 	public static void main(String[] args) {
 		OopService oop = new OopService();
-		oop.test();
+		oop.doStuff();
 	}
 
 }
--- intellij/oop-demo/src/main/java/com/revature/services/OopService.java
+++ intellij/oop-demo/src/main/java/com/revature/services/OopService.java
@@ -1,29 +1,16 @@
 package com.revature.services;
 
+import java.time.LocalDate;
+import com.revature.models.*;
+
 public class OopService {
     private int i;
     protected String s;
     String something; // default access modifier
 
     public void doStuff(){
-        System.out.println(User.getNumOfUsers());
-        User u = new User();
-        User k = new User();
-        u.setAge(-25);
-        Object o = new Object(); // o is a reference to an object/instance of the Object class
-        System.out.println("u age: " + u.getAge());
-        System.out.println(k);
-        System.out.println("Is k.equals(u)? " + k.equals(u));
-        Task t1 = new Task("Laundry", LocalDate.parse("2021-10-20"));
-        System.out.println(t1);
-        u.setTask(t1);
-        System.out.println(u);
-        ImportantTask it = new ImportantTask("Quiz", LocalDate.now());
-        it.stress();
-        System.out.println(it);
-    }
-
-    public void test(){
-        System.out.println("test successful");
+        BoringTask bt = new BoringTask("inheritance", LocalDate.now());
+        bt.procrastinate();
+        System.out.println(bt.getDueDate());
     }
 }
