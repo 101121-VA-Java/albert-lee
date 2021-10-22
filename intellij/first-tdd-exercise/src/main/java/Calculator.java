e991a138a36530a8c235a6201c8f42fa26b5f7c9
Albert Lee <albert.lee@alumni.stevens.edu>
2021-10-22 17:29:19 -0400

    first tdd exercise solution

---
+++ intellij/first-tdd-exercise/src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    static int add(int x, int y) {
+        return x + y;
+    }
+
+    static int subtract(int x, int y) {
+        return x - y;
+    }
+
+    static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    static int divide(int x, int y) {
+        return x / y;
+    }
+}
