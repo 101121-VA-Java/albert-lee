3e4e673f53f5ced83bdb3e62558a679e94f01510
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-11-07 16:31:09 -0500

    add tests for user services

--- intellij/adventurers-shop/src/test/java/com/revature/services/UserServiceTest.java
+++ intellij/adventurers-shop/src/test/java/com/revature/services/UserServiceTest.java
@@ -1,5 +1,45 @@
 package com.revature.services;
 
+import com.revature.exceptions.InvalidRole;
+import com.revature.exceptions.UserNotFoundException;
+import com.revature.models.User;
+import com.revature.repositories.UserPostgres;
+import org.junit.jupiter.api.*;
+
 class UserServiceTest {
+    private static UserService us;
+    private static UserPostgres up;
+
+
+    @BeforeAll
+    public static void setup(){
+        us = new UserService();
+        up = new UserPostgres();
+        up.delete(2);
+    }
+
+    @Test
+    void register() {
+        User newUser = new User("username", "password");
+        int actual = 0;
+        try {
+            actual = us.register(newUser, "1");
+        } catch (InvalidRole e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(actual > 0);
+    }
+
+    @Test
+    void login() {
+        int expectedUserId = 1;
+        int actualUserId = 0;
+        try {
+            actualUserId = us.login("1", "1").getId();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertEquals(actualUserId, expectedUserId);
+    }
 
 }
