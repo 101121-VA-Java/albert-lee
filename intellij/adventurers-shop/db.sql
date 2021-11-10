da314fe7188e1418955cf7908d98633ea4e4e589
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-11-09 22:48:52 -0500

    add warning for seed instructions due to bcrypt

--- intellij/adventurers-shop/db.sql
+++ intellij/adventurers-shop/db.sql
@@ -1,3 +1,5 @@
+-- WARNING: gcreate username 1 and 2 in console after creating tables but before seeding data, because BCrypt generates the stored password
+
 drop table if exists payments;
 drop table if exists offers;
 drop table if exists items;
