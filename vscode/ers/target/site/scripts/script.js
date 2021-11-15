814d8a0c24007a93e8de06e52edcfaa158c7e393
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-11-15 14:47:36 -0500

    pokemon exercise

--- vscode/ers/target/site/scripts/script.js
+++ vscode/ers/target/site/scripts/script.js
@@ -1,3 +1,6 @@
-function hw(){
-    console.log("hello world!");
-}
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+let response = fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => response = data)
+    .catch(err => console.log(err));
