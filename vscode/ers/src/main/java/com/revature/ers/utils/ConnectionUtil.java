18204f1577db0385a460c4591e3d3018e477c037
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-11-16 12:48:49 -0500

    add connection util file

---
+++ vscode/ers/src/main/java/com/revature/ers/utils/ConnectionUtil.java
@@ -0,0 +1,24 @@
+package com.revature.ers.utils;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static Connection con;
+
+    public static Connection getConnectionFromFile() throws IOException, SQLException {
+        if(con == null || con.isClosed()) {
+            Properties prop = new Properties();
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            prop.load(loader.getResourceAsStream("prop.properties"));
+            String url = prop.getProperty("url");
+            String username = prop.getProperty("username");
+            String password = prop.getProperty("password");
+            con = DriverManager.getConnection(url, username, password);
+        }
+        return con;
+    }
+}
