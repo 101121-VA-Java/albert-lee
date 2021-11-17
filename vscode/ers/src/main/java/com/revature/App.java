16b9a1492546d9e1febb882401982a01bb702660
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-11-16 23:33:12 -0500

    initial javalin setup with static files configured

--- vscode/ers/src/main/java/com/revature/App.java
+++ vscode/ers/src/main/java/com/revature/App.java
@@ -1,13 +1,21 @@
 package com.revature;
 
-/**
- * Hello world!
- *
- */
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/site", Location.CLASSPATH);
+		});
+		
+		app.start();
+		
+		app.get("hello", (ctx) -> {
+			ctx.result("hello");
+		});
     }
 }
