ebadc595b3adbdf04ecf3e5fbd50c3c0ccdeb39f
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-11-19 11:04:08 -0500

    add reimbursement form and hard code functionality

--- projects/ers/frontend/scripts/script.js
+++ projects/ers/frontend/scripts/script.js
@@ -1,8 +1,8 @@
-function getEmployees(){
+function getEmployees() {
     let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if( xhr.readyState === 4){
-            if(xhr.status >= 200 && xhr.status < 300){
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status >= 200 && xhr.status < 300) {
                 let response = xhr.response;
                 response = JSON.parse(response);
                 console.log(response);
@@ -13,7 +13,7 @@ function getEmployees(){
     xhr.send();
 }
 
-async function register(){
+async function register() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let email = document.getElementById("email").value;
@@ -21,12 +21,12 @@ async function register(){
     let lastName = document.getElementById("lastName").value;
     let newUser = { username, password, email, firstName, lastName };
     let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if( xhr.readyState === 4){
-            if(xhr.status === 201){
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 201) {
                 console.log('Employee was successfully added!');
                 login();
-            } else{
+            } else {
                 console.log('Employee was not added...');
             }
         }
@@ -36,21 +36,21 @@ async function register(){
     xhr.send(requestBody);
 }
 
-function login(){
+function login() {
     document.getElementById("error-div").innerHTML = "";
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/auth");
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4 && xhr.status === 200){
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             let authToken = xhr.getResponseHeader("Authorization");
             sessionStorage.setItem("token", authToken);
             getDashboard();
-        } else if (xhr.readyState === 4){
+        } else if (xhr.readyState === 4) {
             document.getElementById("error-div").innerHTML = "Unable to login.";
         }
-    } 
+    }
 
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     let requestBody = `username=${username}&password=${password}`;
@@ -63,7 +63,7 @@ let forceAuth = () => {
     let ok = ["login_form", "register_form", "index"];
     let safe = ok.some(url => ref.includes(url));
     if (!token && !safe)
-       window.location.href = "/frontend/html/index.html";
+        window.location.href = "/frontend/html/index.html";
 }
 
 let getDashboard = () => {
@@ -71,22 +71,62 @@ let getDashboard = () => {
     let ref = window.location.href;
     let ok = ["dashboard", "index"];
     let safe = ok.some(url => ref.includes(url));
-    if(token && !safe){
+    if (token && !safe) {
         let d = "";
-        if(token.split(":")[1] === "MANAGER"){
+        if (token.split(":")[1] === "MANAGER") {
             d = "manager_dashboard";
-        } else if(token.split(":")[1] === "BASIC"){
+        } else if (token.split(":")[1] === "BASIC") {
             d = "employee_dashboard";
-        } else if(token.split(":")[1] === "ADMIN"){
+        } else if (token.split(":")[1] === "ADMIN") {
             d = "admin_dashboard";
         }
-        if(d.length > 0) window.location.href = `/frontend/html/${d}.html`;
+        if (d.length > 0) window.location.href = `/frontend/html/${d}.html`;
+    }
+}
+
+let setUpReimbursementForm = (container = document.getElementById("employee-dash")) => {
+    //amount, description
+
+    //java will add the values below after the http req is sent
+    //submissionTime, 
+
+    //receipt image is optional after mvp
+    if (container) container.innerHTML = 
+    `<form id="register-form" onsubmit="return false">
+        New Reimbursement Claim
+        <div class="form-group">
+            <label for="amount">Amount</label>
+            <input type="number" class="form-control" id="reimb-amount" aria-describedby="amountHelp"
+                placeholder="Amount here as a number">
+        </div>
+        <div class="form-group">
+            <label for="text">Description</label>
+            <input type="text" class="form-control" id="reimb-description" placeholder="Description in 250 characters or less">
+        </div>
+        <button onclick={addReimbursement()} class="btn btn-primary">Submit</button>
+    </form>
+    <div id="error-div"></div>`;
+}
+
+let addReimbursement = () => {
+    console.log('reimbursement added');
+}
+
+let getReimbursements = () => {
+    container = document.getElementById("employee-dash");
+    if(container){
+        container.innerHTML = 
+        `<ul>
+            <div>hardcoded reimbursement 1</div>
+            <div>hardcoded reimbursement 2</div>
+            <div>hardcoded reimbursement 3</div>
+        </ul>`
     }
 }
 
 let logout = () => {
-   sessionStorage.clear();
-   window.location.href = "/frontend/html/index.html";
+    sessionStorage.clear();
+    window.location.href = "/frontend/html/index.html";
 }
 
 forceAuth();
